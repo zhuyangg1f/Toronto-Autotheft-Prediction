88e51116c761cf66f85ddf9b4d29762e916ece57
zhuyangg1f <48567915+zhuyangg1f@users.noreply.github.com>
2020-09-20 21:16:57 -0400

    init_project, create_data_cleaning.R

---
+++ dev/data_cleaning.R
@@ -0,0 +1,33 @@
+# Data cleaning --------------------------------------------------------------
+# Yang Zhu
+
+# load packages --------------------------------------------------------------
+library(dplyr)
+library(lubridate)
+
+# load data ------------------------------------------------------------------
+dt <- readRDS("~/Desktop/Toronto police case/Toronto-Autotheft-Prediction/data/auto_theft_data.rdata")
+glimpse(dt)
+
+# check duplicated index
+length(unique(dt$Index_)) # keep all
+length(unique(dt$event_unique_id)) # There are duplicated event id
+length(unique(dt$ObjectId))
+
+duplicated_rows <- dt %>% 
+  filter(event_unique_id %in% dt$event_unique_id[duplicated(dt$event_unique_id)]) %>% 
+  arrange(event_unique_id)
+
+# X, Y, Index_, ObjectId, ucr_code, ucr_ext are not useful remove
+dt <- dt %>% 
+  select(-c(X, Y, Index_, ObjectId, ucr_code, ucr_ext)) %>% 
+  distinct()
+
+# transform date colum into date object
+dt <- dt %>% 
+  mutate(occurrence_ts = ymd_hms(occurrencedate),
+         reported_ts = ymd_hms(reporteddate)) %>% 
+  select(-occurrencedate, -reporteddate)
+
+# save data as data_clean.rdata
+saveRDS(dt, "~/Desktop/Toronto police case/Toronto-Autotheft-Prediction/data/autotheft_clean.rdata")
