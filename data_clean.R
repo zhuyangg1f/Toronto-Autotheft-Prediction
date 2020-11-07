20be61ef0cc20ce5fd6272201a5a01ee2bc09bcf
Hanyu Qi <71859460+hanyuqi77@users.noreply.github.com>
2020-11-07 02:00:18 -0500

    Updated on Nov.7 1:59AM

--- data_clean.R
+++ data_clean.R
@@ -1,4 +1,3 @@
-library(weathercan)
 library(dplyr)
 library(lubridate)
 library(stringr)
@@ -11,7 +10,7 @@ neighborhood <- read.csv('/Users/mac/Documents/Toronto-Autotheft-Prediction-data
 dt <- readRDS("/Users/mac/Documents/Toronto-Autotheft-Prediction-data_exploration/data/auto_theft_data.rdata")
 
 # Select Relative Columns from Raw Neighborhood Data Frame
-generate_auto_neighbor_dt <- function(auto_dt, raw_neighborhood_dt) {
+clean_auto_neighbor_dt <- function(raw_neighborhood_dt) {
   
   # Delete columns that are not useful
   data <- raw_neighborhood_dt %>% 
@@ -29,59 +28,47 @@ generate_auto_neighbor_dt <- function(auto_dt, raw_neighborhood_dt) {
   i = c(1:9)
   ppl_2016[,i] = apply(ppl_2016[ , i], 2, function(x) as.numeric(x))
   
-  # Add estimated population for 2017, 2018, 2019
-  ppl_2017 <- ppl_2016 %>% 
-    dplyr:: mutate(ppl_2017 = round(ppl_2016 * (1+annual_growth_rate)^1))
-  ppl_2018 <- ppl_2016 %>% 
-    dplyr:: mutate(ppl_2018 = round(ppl_2016 * (1+annual_growth_rate)^2))
-  ppl_2016 <- ppl_2016 %>% 
-    dplyr:: mutate(ppl_2019 = round(ppl_2016 * (1+annual_growth_rate)^3))
-  
   
   # Estimate population for different ages in 2017
+  ppl_2017 <- ppl_2016 %>% 
+    dplyr:: transmute(ppl_2017 = round(ppl_2016 * (1+annual_growth_rate)^1))
+  
   ppl_2017[,c('children_2017','youth_2017','working_2017','pre.retirement_2017','seniors_2017','older_2017')] = NA
-  col_num = c(3,4,5,6,7,8)
+  col_num = c(2,3,4,5,6,7)
   
   for (i in col_num) {
-    ppl_2017[i] = round(ppl_2016[i+1] * (1+ppl_2016[3]))
+    ppl_2017[i] = round(ppl_2016[i+2] * (1+ppl_2016[3]))
   }
   
   # Estimate population for different ages in 2018
+  ppl_2018 <- ppl_2016 %>% 
+    dplyr:: transmute(ppl_2018 = round(ppl_2016 * (1+annual_growth_rate)^2))
+  
   ppl_2018[,c('children_2018','youth_2018','working_2018','pre.retirement_2018','seniors_2018','older_2018')] = NA
-  col_num = c(3,4,5,6,7,8)
+  col_num = c(2,3,4,5,6,7)
   
   for (i in col_num) {
-    ppl_2018[i] = round(ppl_2016[i+1] * (1+ppl_2016[3])^2)
+    ppl_2018[i] = round(ppl_2016[i+2] * (1+ppl_2016[3])^2)
   }
   
   # Estimate population for different ages in 2019
+  ppl_2019 <- ppl_2016 %>% 
+    dplyr:: transmute(ppl_2019 = round(ppl_2016 * (1+annual_growth_rate)^3))
+  
   ppl_2019[,c('children_2019','youth_2019','working_2019','pre.retirement_2019','seniors_2019','older_2019')] = NA
-  col_num = c(3,4,5,6,7,8)
+  col_num = c(2,3,4,5,6,7)
   
   for (i in col_num) {
-    ppl_2019[i] = round(ppl_2016[i+1] * (1+ppl_2016[3])^3)
+    ppl_2019[i] = round(ppl_2016[i+2] * (1+ppl_2016[3])^3)
   }
   
-  # Link each year to occurrenceyear
-  auto_dt_neighbor_16 <- auto_dt %>% filter(occurrenceyear == 2016) %>% 
-    dplyr::left_join(ppl_2016 , by = 'Hood_ID')
-  
-  auto_dt_neighbor_17 <- auto_dt %>% filter(occurrenceyear == 2017) %>% 
-    dplyr::left_join(ppl_2017 , by = 'Hood_ID')
-  
-  auto_dt_neighbor_18 <- auto_dt %>% filter(occurrenceyear == 2018) %>% 
-    dplyr::left_join(ppl_2018 , by = 'Hood_ID')
-  
-  auto_dt_neighbor_19 <- auto_dt %>% filter(occurrenceyear == 2019) %>% 
-    dplyr::left_join(ppl_2019 , by = 'Hood_ID')
-  
-  comb1 = merge(auto_dt_neighbor_16, auto_dt_neighbor_17,all  = TRUE)
-  comb2 = merge(comb1, auto_dt_neighbor_18, all=TRUE)
-  comb3 = merge(comb2,auto_dt_neighbor_19,all= TRUE)
-  
-  return(comb3)
+ comb = cbind(ppl_2016,ppl_2017,ppl_2018,ppl_2019)
+  return(comb)
   
 }
+
+neighbor_data = clean_auto_neighbor_dt(neighborhood)
+
 
 # Select Relative Columns from Raw Auto Data Frame
 clean_auto_dt <- function(raw_auto_dt) {
@@ -106,17 +93,21 @@ auto_dt <- clean_auto_dt(dt)
 # Join Neighborhood Data Into Auto Data
 join_auto_neighbor <- function(auto_data, neighbor_data) {
   
+  neighbor_2016 = neighbor_data[1:9]
   auto_dt_neighbor_16 <- auto_dt %>% filter(occurrenceyear == 2016) %>% 
-    dplyr::left_join(ppl_2016 , by = 'Hood_ID')
+    dplyr::left_join(neighbor_2016 , by = 'Hood_ID')
   
+  neighbor_2017 = neighbor_data[c(1,10:16)]
   auto_dt_neighbor_17 <- auto_dt %>% filter(occurrenceyear == 2017) %>% 
-    dplyr::left_join(ppl_2017 , by = 'Hood_ID')
+    dplyr::left_join(neighbor_2017 , by = 'Hood_ID')
   
+  neighbor_2018 = neighbor_data[c(1,17:23)]
   auto_dt_neighbor_18 <- auto_dt %>% filter(occurrenceyear == 2018) %>% 
-    dplyr::left_join(ppl_2018 , by = 'Hood_ID')
+    dplyr::left_join(neighbor_2018 , by = 'Hood_ID')
   
+  neighbor_2019 = neighbor_data[c(1,24:30)]
   auto_dt_neighbor_19 <- auto_dt %>% filter(occurrenceyear == 2019) %>% 
-    dplyr::left_join(ppl_2019 , by = 'Hood_ID')
+    dplyr::left_join(neighbor_2019 , by = 'Hood_ID')
   
   comb1 = merge(auto_dt_neighbor_16, auto_dt_neighbor_17,all  = TRUE)
   comb2 = merge(comb1, auto_dt_neighbor_18, all=TRUE)
@@ -125,7 +116,7 @@ join_auto_neighbor <- function(auto_data, neighbor_data) {
   return(comb3)
 }
 
-auto_dt_neighbor = generate_auto_neighbor_dt(auto_dt,neighborhood)
+auto_dt_neighbor = join_auto_neighbor(auto_dt,neighbor_data)
 write.csv(auto_dt_neighbor,"/Users/mac/Documents/Toronto-Autotheft-Prediction-data_exploration/data/auto_neighbor.csv")
 saveRDS(auto_dt_neighbor, "/Users/mac/Documents/Toronto-Autotheft-Prediction-data_exploration/data/auto_neighbor_dt.rdata")
 
