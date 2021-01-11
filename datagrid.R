1cffee5f904f4b10cb0862864005adac1501f227
Hanyu Qi <71859460+hanyuqi77@users.noreply.github.com>
2021-01-11 11:25:14 -0500

    Add files via upload

--- datagrid.R
+++ datagrid.R
@@ -2,8 +2,8 @@ data = read.csv('/Users/mac/Documents/Toronto-Autotheft-Prediction-dev/data/clea
 ## Add the latitudes and longitudes between which each observation is located
 ## You can substitute any number of breaks you want. Or, a vector of fixed cutpoints
 ## LATgrid and LONgrid are going to be factors. With ugly level names.
-data$LATgrid<-cut(data$Lat,breaks=10,include.lowest=T);
-data$LONgrid<-cut(data$Long,breaks=10,include.lowest=T);
+data$LATgrid<-cut(data$Lat,breaks=45,include.lowest=T);
+data$LONgrid<-cut(data$Long,breaks=45,include.lowest=T);
 
 ## Create a single factor that gives the lat,long of each observation. 
 data$IDgrid<-with(data,interaction(LATgrid,LONgrid));
@@ -24,6 +24,9 @@ aggregate(data$Lat,data[,c('LATgrid','LONgrid','IDNgrid')],FUN=length);
 
 ## If you want a really minimalist table, you could do this:
 tb = table(data$IDNgrid);
-View(tb)
+par(mfrow=c(1,2))
+plot.default(data$LATgrid,data$LONgrid, main="grid data")
+
+plot.default(data$Lat,data$Long, main="raw data")
 
 
